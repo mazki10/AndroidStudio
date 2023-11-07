f31c8b68dd16d338436670b7758c3228b9cabef6
mazki10 <mazkigazte@gmail.com>
2023-11-07 13:23:26 +0100

    coño

---
+++ AndroidBeniat/app/src/main/java/com/example/androidbeniat/Actividad_Secundaria_Beniat.java
@@ -0,0 +1,14 @@
+package com.example.androidbeniat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Actividad_Secundaria_Beniat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad_secundaria_beniat);
+    }
+}
