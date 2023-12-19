7050e4d4c58d965d92ca4ff6b04157387b6d22f1
mazki10 <mazkigazte@gmail.com>
2023-12-19 05:54:21 +0100

    copias del examen

---
+++ recursos/Fragmentos/Actividad_Secundaria_Beniat.java
@@ -0,0 +1,153 @@
+package com.example.androidbeniat;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Actividad_Secundaria_Beniat extends AppCompatActivity {
+    private EditText ing;
+    private Button botRes, botLim, botSal;
+    private RadioButton snHi, unHi, maHi;
+
+    private RadioGroup gru;
+
+    String nombre, ape1, ape2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad_secundaria_beniat);
+
+        ing = findViewById(R.id.etn2);
+
+        botRes = findViewById(R.id.bt3);
+        botLim = findViewById(R.id.bt4);
+        botSal = findViewById(R.id.bt5);
+
+        snHi = findViewById(R.id.rb1);
+        unHi = findViewById(R.id.rb2);
+        maHi = findViewById(R.id.rb2);
+
+        gru = findViewById(R.id.radioGroup);
+
+        botLim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                limpiar(v);
+            }
+        });
+
+        botRes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                comprobar(v);
+            }
+        });
+
+        botSal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                salir(v);
+            }
+        });
+
+        Intent intent = getIntent();
+        nombre = intent.getStringExtra("Nombre");
+        ape1 = intent.getStringExtra("Apellido1");
+        ape2 = intent.getStringExtra("Apellido2");
+
+    }
+
+    public void limpiar(View v) {
+
+        ing.setText("");
+        gru.clearCheck();
+    }
+
+    public boolean evaluarSolicitud(int numerosHijos, Double ingresoAnual) {
+
+        Boolean aceptado = false;
+
+        if (ingresoAnual < 9000) {
+            aceptado = true;
+        } else if (ingresoAnual >= 9001 && ingresoAnual <= 20000 && numerosHijos >= 1) {
+            aceptado = true;
+        } else if (ingresoAnual >= 20001 && ingresoAnual <= 30000 && numerosHijos > 1) {
+            aceptado = true;
+        }
+
+        return aceptado;
+    }
+
+    public void comprobar(View v) {
+
+        String ingresos = ing.getText().toString();
+        int selectedRadioButtonId = gru.getCheckedRadioButtonId();
+
+
+        if (TextUtils.isEmpty(ingresos) || selectedRadioButtonId == -1) {
+            dialogRellenar(v);
+        } else {
+            mostrarSolicitud(v);
+        }
+
+    }
+
+    public void dialogRellenar(View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Vacio")
+                .setMessage("ERROR: Debe rellenar todos los campos")
+                .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {}
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    public void mostrarSolicitud(View v) {
+        Double ingresos = Double.parseDouble(ing.getText().toString());
+        int hijos = 0;
+
+        if (unHi.isChecked()) {
+            hijos = 0;
+        } else {
+            if (unHi.isChecked()) {
+                hijos = 1;
+            } else {
+                if (maHi.isChecked()) {
+                    hijos = 3;
+                }
+            }
+        }
+
+        String resultado;
+
+        Boolean acept = evaluarSolicitud(hijos, ingresos);
+
+        if (acept) {
+            resultado = "ACEPTADO";
+        } else {
+            resultado = "DENEGADO";
+        }
+
+        String estructuraSolicitud = "La solicitud de " + nombre + " " + ape1 + " " + ape2 + " a sido " + resultado;
+
+        Toast.makeText(this, estructuraSolicitud, Toast.LENGTH_SHORT).show();
+    }
+
+    public void salir(View v) {
+        finish();
+    }
+}
---
+++ recursos/Fragmentos/MainActivity.java
@@ -0,0 +1,138 @@
+package com.example.androidbeniat;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText tNom, aPe1, aPe2, aNum;
+    private Button botSig, botLim;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Declaro los EditTexts
+        tNom = findViewById(R.id.et1);
+        aPe1 = findViewById(R.id.et2);
+        aPe2 = findViewById(R.id.et3);
+        aNum = findViewById(R.id.etn1);
+
+        // Declaro los botones
+        botSig = findViewById(R.id.bt1);
+        botLim = findViewById(R.id.bt2);
+
+        // Declaro los eventos OnClick
+        botSig.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                comprobar(v);
+            }
+        });
+
+        botLim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                limpiar(v);
+            }
+        });
+    }
+
+    public void limpiar(View v) {
+        // Vacío los EditTexts
+        tNom.setText("");
+        aPe1.setText("");
+        aPe2.setText("");
+        aNum.setText("");
+        tNom.requestFocus();
+    }
+
+    public void comprobar(View v) {
+        String nombre = tNom.getText().toString();
+        String ape1 = aPe1.getText().toString();
+        String ape2 = aPe2.getText().toString();
+        String anio = aNum.getText().toString();
+
+        if (TextUtils.isEmpty(nombre) || TextUtils.isEmpty(ape1) || TextUtils.isEmpty(ape2) || TextUtils.isEmpty(anio)) {
+            dialogRellenar(v);
+        } else {
+            comprobarEdad(v);
+        }
+    }
+
+    public void dialogRellenar(View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Vacio")
+                .setMessage("ERROR: Debe rellenar todos los campos")
+                .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    public void dialogAnio(View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Formato de año incorrecto")
+                .setMessage("ERROR: El año debe estar entre 1900 y el año actual.")
+                .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        aNum.setText("");
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    public void comprobarEdad(View v) {
+        int anioInt = Integer.parseInt(aNum.getText().toString());
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+
+        if (anioInt < 1900 || anioInt > year) {
+            dialogAnio(v);
+        } else {
+            int edad = calcularEdad(anioInt);
+            if (edad < 18 || edad > 65) {
+                Toast.makeText(this, "No cumples con los requisitos mínimos de edad", Toast.LENGTH_SHORT).show();
+                aNum.setText("");
+            } else {
+                pasa(v);
+            }
+        }
+    }
+
+    public int calcularEdad(int anio) {
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        return year - anio;
+    }
+
+    public void pasa(View v) {
+        String nombre = tNom.getText().toString();
+        String ape1 = aPe1.getText().toString();
+        String ape2 = aPe2.getText().toString();
+
+        Intent intent = new Intent(this, Actividad_Secundaria_Beniat.class);
+        intent.putExtra("Nombre", nombre);
+        intent.putExtra("Apellido1", ape1);
+        intent.putExtra("Apellido2", ape2);
+        startActivity(intent);
+    }
+}
